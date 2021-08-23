396f65fabdd337ca0c5e6e6d78df7dfc6c93af01
Eti Daniel <danieleti@gmail.com>
2021-08-23 00:12:32 -0400

    adding fetchCoordsByIp to get the coordinates

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //This file will require and run our main fetch function.
 
 //const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -9,4 +10,13 @@
 //   }
 
 //   console.log('It worked! Returned IP:' , ip);
+// });
+
+// fetchCoordsByIP('72.140.26.8', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned coordinates:' , coordinates);
 // });
--- iss.js
+++ iss.js
@@ -28,4 +28,23 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function (ip, callback) {
+
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    // error can be set if invalid domain, user is offline, etc.
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      return;
+    }
+    //if we are successful let's send the coordinates
+    const { latitude, longitude } = JSON.parse(body);
+    callback(null, { latitude, longitude });
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
